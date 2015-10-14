61af6e46badbeee14cc074ffd276d57c2f22d2fe
Max Nowack <max.nowack@oafmedium.de>
2015-10-14 09:35:02 +0200

    fix dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maximum:reactive-cursors',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'Meteor package that provides reactive cursors on the server',
   git: 'https://github.com/maximummeteor/reactive-cursors',
   documentation: 'README.md'
@@ -11,8 +11,15 @@ Package.onUse(function(api) {
   api.use([
     'coffeescript',
     'mongo',
+    'underscore',
     'lai:collection-extensions@0.1.3',
-    'peerlibrary:server-autorun@0.2.3'
+    'peerlibrary:server-autorun@0.5.1',
+    'tracker'
+  ]);
+
+  api.imply([
+    'peerlibrary:server-autorun@0.5.1',
+    'tracker'
   ]);
 
   api.addFiles([
@@ -24,6 +31,8 @@ Package.onTest(function(api) {
   api.use([
     'tinytest',
     'coffeescript',
+    'mongo',
+    'underscore',
     'maximum:reactive-cursors'
   ]);
   api.addFiles('tests/reactive-cursors.coffee');
